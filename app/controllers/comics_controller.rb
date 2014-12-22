0b6e62c1b74d7325c1d537b552ee5efea317adef
Jamie Francese <jamienf@gmail.com>
2014-12-22 17:28:28 -0500

    Added more comics

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,9 +1,10 @@
 class ComicsController < ApplicationController
   def index
     @comic_list = []
-    # @comic_list << ComicScraper.xkcd_image
-    # @comic_list << ComicScraper.explosm_image_url
-    # @comic_list << ComicScraper.penny_arcade_url
+    @comic_list << ComicScraper.buttersafe_info
+    @comic_list << ComicScraper.dino_info
+    @comic_list << ComicScraper.explosm_info
+    @comic_list << ComicScraper.smbc_info
     @comic_list << ComicScraper.xkcd_info
   end
 end
--- app/models/comic_scraper.rb
+++ app/models/comic_scraper.rb
@@ -1,14 +1,70 @@
 class ComicScraper
 
-  def self.explosm_image_url
+  ###############################
+  #######   Buttersafe   ########
+  ###############################
+
+  def self.buttersafe_info
+    explosm = {"name" => "Buttersafe",
+      "image" => buttersafe_image,
+      "archive" => "http://buttersafe.com/archive/"}
+    explosm
+  end
+
+  def self.buttersafe_image
+    doc = Nokogiri::HTML(open('http://buttersafe.com/'))
+    src = doc.css("div#comic img").attr('src')
+    "#{src}"
+  end
+
+  ###############################
+  ####   Dinosaur Comcis   ######
+  ###############################
+
+  def self.dino_info
+    explosm = {"name" => "Dinosaur Comics",
+      "image" => dino_image,
+      "archive" => "http://www.qwantz.com/archive.php"}
+    explosm
+  end
+
+  def self.dino_image
+    doc = Nokogiri::HTML(open('http://www.qwantz.com/index.php'))
+    src = doc.css("center img").attr('src')
+    "#{src}"
+  end
+
+  ###############################
+  ########   explosm   ##########
+  ###############################
+
+  def self.explosm_info
+    explosm = {"name" => "Explosm",
+      "image" => explosm_image,
+      "archive" => "http://explosm.net/comics/archive"}
+    explosm
+  end
+
+  def self.explosm_image
     doc = Nokogiri::HTML(open('http://explosm.net/comics/latest'))
     src = doc.css("div#comic-container img").attr('src')
     "#{src}"
   end
 
-  def self.penny_arcade_url
-    doc = Nokogiri::HTML(open('http://www.penny-arcade.com/comic'))
-    src = doc.css("div#comicFrame img").attr('src')
+  ###############################
+  ##########   SMBC   ###########
+  ###############################
+
+  def self.smbc_info
+    explosm = {"name" => "Saturday Morning Breakfast Cereal",
+      "image" => smbc_image,
+      "archive" => "http://www.smbc-comics.com/archives.php"}
+    explosm
+  end
+
+  def self.smbc_image
+    doc = Nokogiri::HTML(open('http://www.smbc-comics.com/'))
+    src = doc.css("div#comicimage img").attr('src')
     "#{src}"
   end
 
@@ -18,16 +74,9 @@ class ComicScraper
 
   def self.xkcd_info
     xkcd = {"name" => "XKCD",
-      "title" => xkcd_title,
       "image" => xkcd_image,
       "archive" => "http://xkcd.com/archive/"}
     xkcd
-  end
-
-  def self.xkcd_title
-    doc = Nokogiri::HTML(open('http://xkcd.com/'))
-    src = doc.css("div#comic img").attr('alt')
-    "#{src}"
   end
 
   def self.xkcd_image
