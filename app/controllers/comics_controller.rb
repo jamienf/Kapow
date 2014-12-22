4e7a1347fb03dc52437d5857366ba5abdbaf05ac
Jamie Francese <jamienf@gmail.com>
2014-12-22 16:38:25 -0500

    created hashes for comic info

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,8 +1,9 @@
 class ComicsController < ApplicationController
   def index
     @comic_list = []
-    @comic_list << ComicScraper.xkcd_image_url
-    @comic_list << ComicScraper.explosm_image_url
-    @comic_list << ComicScraper.penny_arcade_url
+    # @comic_list << ComicScraper.xkcd_image
+    # @comic_list << ComicScraper.explosm_image_url
+    # @comic_list << ComicScraper.penny_arcade_url
+    @comic_list << ComicScraper.xkcd_info
   end
 end
--- app/models/comic_scraper.rb
+++ app/models/comic_scraper.rb
@@ -1,19 +1,39 @@
 class ComicScraper
-  def self.xkcd_image_url(page_url = 'http://xkcd.com/')
-    doc = Nokogiri::HTML(open(page_url))
-    src = doc.css("div#comic img").attr('src')
-    "#{src}"
-  end
 
-  def self.explosm_image_url(page_url = 'http://explosm.net/comics/latest')
-    doc = Nokogiri::HTML(open(page_url))
+  def self.explosm_image_url
+    doc = Nokogiri::HTML(open('http://explosm.net/comics/latest'))
     src = doc.css("div#comic-container img").attr('src')
     "#{src}"
   end
 
-  def self.penny_arcade_url(page_url = 'http://www.penny-arcade.com/comic')
-    doc = Nokogiri::HTML(open(page_url))
+  def self.penny_arcade_url
+    doc = Nokogiri::HTML(open('http://www.penny-arcade.com/comic'))
     src = doc.css("div#comicFrame img").attr('src')
     "#{src}"
   end
+
+  ###############################
+  ##########   XKCD   ###########
+  ###############################
+
+  def self.xkcd_info
+    xkcd = {"name" => "XKCD",
+      "title" => xkcd_title,
+      "image" => xkcd_image,
+      "archive" => "http://xkcd.com/archive/"}
+    xkcd
+  end
+
+  def self.xkcd_title
+    doc = Nokogiri::HTML(open('http://xkcd.com/'))
+    src = doc.css("div#comic img").attr('alt')
+    "#{src}"
+  end
+
+  def self.xkcd_image
+    doc = Nokogiri::HTML(open('http://xkcd.com/'))
+    src = doc.css("div#comic img").attr('src')
+    "#{src}"
+  end
+
 end
